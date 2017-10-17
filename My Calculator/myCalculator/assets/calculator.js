c30c8e1d91a53ff78d64cdda170ff7e9f0158778
Patrick-Raphael Perez <patrickraphaelperez@gmail.com>
2017-10-17 12:54:39 -0700

    angular implementation
    defined angularJS app and controller, set up the select options, set default input values, and set variables for conversion values

--- My Calculator/myCalculator/assets/calculator.js
+++ My Calculator/myCalculator/assets/calculator.js
@@ -1,5 +1,30 @@
 /* JavaScript Document */
 
-/* Script goes here */
+(function(){
+
+	var app = angular.module('myCalculator',[]);
+
+	app.controller('CalculatorController',['$scope', function($scope){
+
+		$scope.lumen_options = [375, 600, 900, 1125, 1600];
+		$scope.current_lumens = 600;
+		$scope.current_cost = 12;
+		$scope.current_hours = 3;
+		$scope.total_days = 365;
+
+		$scope.inc_conversion = .0625;
+		$scope.hal_conversion = .0450;
+		$scope.cfl_conversion = .0146;
+		$scope.led_conversion = .0125;
+
+	}]);
+
+
+
+
+
+
+
+})();
 
 
